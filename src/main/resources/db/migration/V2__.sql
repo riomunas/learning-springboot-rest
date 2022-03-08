914066821c69cb183521e963e25f485ee997c342
rio <rio@notebook>
2022-03-08 09:40:53 +0700

    buat script insert ke app_user

---
+++ src/main/resources/db/migration/V2__.sql
@@ -0,0 +1,12 @@
+INSERT
+INTO
+  app_user
+  (sid, name)
+VALUES
+  (NEXTVAL('hibernate_sequence'), 'Jane White');
+INSERT
+INTO
+  app_user
+  (sid, name)
+VALUES
+  (NEXTVAL('hibernate_sequence'), 'Harry Blue');
